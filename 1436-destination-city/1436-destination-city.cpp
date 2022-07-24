2412d2fe7c14f52f18d356b4c4146dedf698e5ac
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-07-24 14:13:56 +0900

    Time: 29 ms (14.95%), Space: 10.1 MB (94.90%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        // int n=paths.size();
+        for(int i=0; i<paths.size(); i++){
+            string dest=paths[i][1];
+            bool is_dest=false;
+            for(int j=0; j<paths.size(); j++){
+                if(paths[j][0]==dest) is_dest=true;
+            }
+            if(!is_dest) return dest;
+        }
+        return "";
+    }
+};
