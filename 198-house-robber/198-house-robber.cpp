462731a1c65eedeb98d24c6999ef6a6e29255610
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-08-10 00:02:16 +0900

    Time: 3 ms (54.99%), Space: 7.9 MB (25.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
    int rob(vector<int>& nums) {
-        int inc=nums[0];
-        int exc=0;
-        for(int i=1;i<nums.size();i++)
-        {
-            int ninc=exc+nums[i];
-            int nexc=max(inc,exc);
-            inc=ninc;
-            exc=nexc;
-        }
-        return max(inc,exc);
+       if(nums.empty()) return 0;
+       vector<int> dp(nums.size()+1, 0);
+       dp[1] = nums[0];
+       for(int i=2; i<nums.size()+1; i++)
+           dp[i] = max(dp[i-2]+nums[i-1], dp[i-1]);
+       return dp[nums.size()];
     }
 };
