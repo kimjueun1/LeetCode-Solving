ed05985001bf3172a491d425386083e9eaa14711
JuEun <86585240+kimjueun1@users.noreply.github.com>
2022-08-09 23:57:22 +0900

    Time: 0 ms (100.00%), Space: 7.6 MB (95.20%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   int rob(vector<int>& nums) {
+        int inc=nums[0];
+        int exc=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            int ninc=exc+nums[i];
+            int nexc=max(inc,exc);
+            inc=ninc;
+            exc=nexc;
+        }
+        return max(inc,exc);
+    }
+};
